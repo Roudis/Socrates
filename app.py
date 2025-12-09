a5b31ac5e60999db99ffd7dcd156dd46fc08e391
roudis <alkisroudis@gmail.com>
2025-12-09 13:51:39 +0100

    refactor: Clean up comments and improve message display logic in Streamlit app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#import langchain dependencies
+
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
@@ -13,17 +13,18 @@ from watsonxlangchain import LangChainInterface
 
 #setup the app title
 st.title('Ask watson')
-
 #setup a session state message variable to hold all the old messages
 if 'messages' not in st.session_state:
     st.session_state.messages = []
+#display all the historical messages
+for message in st.session_state.messages:
+    st.chat_message(message["role"]).markdown(message["content"])
 
 #build a prompt input template to display the prompts
 prompt = st.chat_input('Pass your Prompt here')
 
 #if the user hits enter
 if prompt:
-    
     #display the prompt
     st.chat_message('user').markdown(prompt)
     #store the use prompt in state
